a47ec6a8d19ac698b619aaad8a4fdf3c062988dd
MrWang98 <w1968953844@163.com>
2020-11-17 17:56:15 +0800

    完善swagger-ui

--- src/main/java/com/example/technologysharing/TechnologySharingApplication.java
+++ src/main/java/com/example/technologysharing/TechnologySharingApplication.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.example.technologysharing.mapper")
+@EnableSwagger2
 public class TechnologySharingApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/technologysharing/config/LoginHandlerInterceptor.java
+++ src/main/java/com/example/technologysharing/config/LoginHandlerInterceptor.java
@@ -12,15 +12,23 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginHandlerInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
         //用户登陆成功后，应该有自己的session
         Object session = request.getSession().getAttribute("LoginUser");
-        if (session == null) {
-            request.setAttribute("msg", "权限不够，请先登录");
-            request.getRequestDispatcher("/index.html").forward(request, response);
-            return false;
-        } else {
-            return true;
-        }
+//        String reqURL = request.getRequestURL().toString();
+//        if (reqURL == "http://121.196.100.176:8080/swagger-ui.html" || reqURL == "http://localhost:8080/swagger-ui.html"){
+//            return true;
+//        }else
+//        if (session == null) {
+//            request.setAttribute("msg", "权限不够，请先登录");
+//            request.getRequestDispatcher("/index.html").forward(request, response);
+//            return false;
+//        } else {
+//            return true;
+//        }
+        return true;
+
+
     }
 
 }
